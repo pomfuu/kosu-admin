9438789552048516da40500b83d9cef021431e20
pomff <alincahyono@gmail.com>
2024-12-26 03:00:20 +0700

    CRUD env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from '@firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
@@ -7,13 +7,12 @@ const firebaseConfig = {
     apiKey: "AIzaSyDF3187eOCbR99ZJFvRi4w92GFiFA0Gcbs",
     authDomain: "teskosudulu.firebaseapp.com",
     projectId: "teskosudulu",
-    storageBucket: "teskosudulu.appspot.com",
+    storageBucket: "teskosudulu.firebasestorage.app",
     messagingSenderId: "328107246588",
     appId: "1:328107246588:web:4fadb134d7d752de60e879"
-};
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { db, app, auth };
+export const db = getFirestore(app);
